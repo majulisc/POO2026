11fcc59e56531fcefc2eb59771e2731fef8602eb
Maria Júlia S Carvalho <mariajuliasouzadecarvalho1@gmail.com>
2026-04-29 20:59:43 -0300

    add: Aluno class e os atributos

---
+++ Aula4/src/Aluno.java
@@ -0,0 +1,7 @@
+public class Aluno {
+    private String nome;
+    private int idade;
+    private int matricula;
+    private boolean[] presenca= null; // o enunciado pede que comece como null 
+
+}
